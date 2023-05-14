d529c935a3a38d2468e95ec719d56759c50341af
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-05-14 12:05:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -45,13 +45,20 @@ const App = () => {
   }
   return (
     <>
-      <div className="ripple-background">
-        <div className="circle xxlarge shade1"></div>
-        <div className="circle xlarge shade2"></div>
-        <div className="circle large shade3"></div>
-        <div className="circle mediun shade4"></div>
-        <div className="circle small shade5"></div>
-      </div>
+      <div class="area" >
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
 
 
       <div className='App'>
